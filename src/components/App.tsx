63af30bfc4a1be68350c577a5d121b063ac05ac4
Justine Licuanan <caissoone@gmail.com>
2021-03-02 18:16:39 +0800

    Setup folder structure

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import { Typography } from '@material-ui/core';
+
+const App = () => {
+	return (
+		<>
+			<Typography variant='h4' align='center'>
+				Hello World
+			</Typography>
+		</>
+	);
+};
+
+export default App;
