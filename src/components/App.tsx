d12a066c91008594632da7893aaf5e5fc5e36a24
Justine Licuanan <caissoone@gmail.com>
2021-03-08 19:13:08 +0800

    Create search bar component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import { ContactCtxProvider } from '../context/ContactCtx';
 import Contacts from './Contacts';
 import Copyright from './Copyright';
+import SearchBar from './SearchBar';
 
 const App = () => {
 	return (
 		<ContactCtxProvider>
+			<SearchBar />
 			<Contacts />
 			<Copyright />
 		</ContactCtxProvider>
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,49 @@
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme: Theme) => {
+	return createStyles({
+		root: {
+			padding: '2px 4px',
+			display: 'flex',
+			alignItems: 'center',
+			width: 400,
+		},
+		input: {
+			marginLeft: theme.spacing(1),
+			flex: 1,
+		},
+		iconButton: {
+			padding: 10,
+		},
+	});
+});
+
+const SearchBar = () => {
+	const classes = useStyles();
+
+	return (
+		<Box py={6} display='flex' justifyContent='center'>
+			<Paper component='form' className={classes.root}>
+				<InputBase
+					className={classes.input}
+					placeholder='Search a contact...'
+					inputProps={{ 'aria-label': 'search contact' }}
+				/>
+				<IconButton
+					type='submit'
+					className={classes.iconButton}
+					aria-label='search'
+				>
+					<SearchIcon />
+				</IconButton>
+			</Paper>
+		</Box>
+	);
+};
+
+export default SearchBar;
